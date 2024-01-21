fdd12a31f2dbbef7643d2ea642fb772db99b8e41
ravikumargh <ravikumargh@gmail.com>
2024-01-21 20:55:18 +0530

    no message

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # java8sample
+
+application will run in 8081 port
+run
+http://localhost:8081
---
+++ src/main/java/com/java8sample/HelloController.java
@@ -0,0 +1,15 @@
+package com.java8sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+     	@GetMapping("/hello")
+        public String greeting() {
+            return "Hello world!";
+        }
+}
